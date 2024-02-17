a6bfe096b3e97f0aff8a4a2876b5cead5b69cb44
SBSHussain <shaikhussain.ece@hotmail.com>
2024-02-17 21:51:34 +0530

    FirstCommit

--- codepulse/src/app/features/category/services/category.service.ts
+++ codepulse/src/app/features/category/services/category.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Category } from '../models/category.model';
+import { environment } from 'src/environments/environment';
+import { UpdateCategoryRequest } from '../models/update-category-request.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +14,22 @@ export class CategoryService {
   constructor(private http: HttpClient) { }
 
   addCategory(model: AddCategoryRequest): Observable<void>{
-    return this.http.post<void>('https://codepulseapii.azurewebsites.net/swagger/index.html', model)
+    return this.http.post<void>(`${environment.apiBaseUrl}/api/Categories`, model)
+  }
+  
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/Categories`);
+  }
 
+  getCategoryById(id:string) : Observable<Category>{
+   return this.http.get<Category>(`${environment.apiBaseUrl}/api/Categories/${id}`);
+  }
+  
+  updateCategory(id: string, updateCategoryRequest: UpdateCategoryRequest) :
+  Observable<Category>{
+    return this.http.put<Category>(`${environment.apiBaseUrl}/api/Categories/${id}`,
+    updateCategoryRequest);
+    
   }
 }
+//https://codepulseapii.azurewebsites.net/swagger/index.html
